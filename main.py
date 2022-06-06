67875727d1ff9a2efee4628d1bbdc58d231d69c4
AstragoDE <>
2022-06-06 21:21:48 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import math
 from sympy import *
 
-x, y = symbols("x y")
+x, y, z = symbols("x y z")
+
+# f, g = symbols("f g", cls=Function)
+
+inEq = [2*x**3 - 15*x**2 + 36*x + 12, 0]
+
+values = []
+for i in solve(Eq(diff(inEq[0], x, 1), inEq[1])):
+    # i = solution of f'(x)
+    pass
