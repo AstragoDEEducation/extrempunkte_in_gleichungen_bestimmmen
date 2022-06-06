0fc36dced02f5069f758332403b3320f36197647
AstragoDE <>
2022-06-06 22:36:14 +0200

    englishified

--- main.py
+++ main.py
@@ -4,40 +4,40 @@ x, y, z = symbols("x y z")
 
 inEq = [2*x**3 - 15*x**2 + 36*x + 12, 0]
 
-ersteAbleitung = Eq(diff(inEq[0], x, 1), inEq[1])
+firstDerivative = Eq(diff(inEq[0], x, 1), inEq[1])
 
-zweiteAbleitungEingesetzt = []
-punkte = []
-for j in solve(ersteAbleitung):
+secondDerivativeInsertedAndSolved = []
+points = []
+for j in solve(firstDerivative):
     # i = solution of f'(x)
 
-    zweiteAbleitung = diff(inEq[0], x, 2)
-    zweiteAbleitung = zweiteAbleitung.subs(x, j)
-    zweiteAbleitungEingesetzt.append(zweiteAbleitung)
+    secondDerivative = diff(inEq[0], x, 2)
+    secondDerivative = secondDerivative.subs(x, j)
+    secondDerivativeInsertedAndSolved.append(secondDerivative)
 
-    eq = inEq[0]
-    eq = eq.subs(x, j)
-    punkte.append([j, eq])
+    baseEquation = inEq[0]
+    baseEquation = baseEquation.subs(x, j)
+    points.append([j, baseEquation])
 
-typ = [None, None]
-if zweiteAbleitungEingesetzt[0] < 0:
-    typ[0] = ("Hochpunkt")
+pointType = [None, None]
+if secondDerivativeInsertedAndSolved[0] < 0:
+    pointType[0] = ("Hochpunkt")
 
-elif zweiteAbleitungEingesetzt[0] > 0:
-    typ[0] = ("Tiefpunkt")
-
-else:
-    typ[0] = ("Sattelpunkt")
-
-
-if zweiteAbleitungEingesetzt[1] < 0:
-    typ[1] = ("Hochpunkt")
-
-elif zweiteAbleitungEingesetzt[1] > 0:
-    typ[1] = ("Tiefpunkt")
+elif secondDerivativeInsertedAndSolved[0] > 0:
+    pointType[0] = ("Tiefpunkt")
 
 else:
-    typ[1] = ("Sattelpunkt")
+    pointType[0] = ("Sattelpunkt")
 
 
-print(f"P({punkte[0][0]}|{punkte[0][1]}) ist ein {typ[0]} (f'' = {zweiteAbleitungEingesetzt[0]}).\nQ({punkte[1][0]}|{punkte[1][1]}) ist ein {typ[1]} (f'' = {zweiteAbleitungEingesetzt[1]}).")
+if secondDerivativeInsertedAndSolved[1] < 0:
+    pointType[1] = ("Hochpunkt")
+
+elif secondDerivativeInsertedAndSolved[1] > 0:
+    pointType[1] = ("Tiefpunkt")
+
+else:
+    pointType[1] = ("Sattelpunkt")
+
+
+print(f"P({points[0][0]}|{points[0][1]}) ist ein {pointType[0]} (f'' = {secondDerivativeInsertedAndSolved[0]}).\nQ({points[1][0]}|{points[1][1]}) ist ein {pointType[1]} (f'' = {secondDerivativeInsertedAndSolved[1]}).")
